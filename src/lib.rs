e218209ace5e1f23b1d1e189bbb99d15eb85e293
Eric Ovenden <eric2ovenden@gmail.com>
2023-01-30 23:15:50 -0600

    fixed documentation in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-/// Module hosting the [Vector][crate::vector::Vector] type and its implementations.
+/// Module hosting the [Vector][crate::vector_impl::Vector] type and its implementations.
 pub mod vector_impl;
 
-/// Module hosting the [Matrix][crate::matrix::Matrix] type and its implementations.
+/// Module hosting the [Matrix][crate::matrix_impl::Matrix] type and its implementations.
 pub mod matrix_impl;
 
-/// Imports the [Vector][crate::vector::Vector] and [Matrix][crate::matrix::Matrix] types.
+/// Imports the [Vector][crate::vector_impl::Vector] and [Matrix][crate::matrix_impl::Matrix] types.
 pub mod prelude;
 
 mod macros;
